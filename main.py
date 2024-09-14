234444f56356a417a3831ae9eeb00212d6d65852
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-09-14 18:17:53 -0500

    Display followup text and take input

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import curses
 from curses import wrapper
 
-# COLOR_GREEN="fff"
-# COLOR_WHITE="fff"
-# COLOR_YELLOW="fff"
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(1, 0, "TYPING BAD: BREAKING LIMITS")
+    stdscr.addstr("\nTime to cook… some serious WPM. Type fast and prove you’re the one who types!")
+    stdscr.addstr("\n\nPress any key to continue..")
+    stdscr.refresh()
+    stdscr.getkey() # waiting to press a key by the user
+
+def wpm_test(stdscr):
+    target_text="Hello world this is some test text for this app!"
+    current_text=""
+    stdscr.clear()
+    stdscr.addstr(1, 1, target_text)
+    stdscr.refresh()
+    stdscr.getkey()
 
 def main(stdscr): # stdscr == stand screen
     # creating color pair
@@ -11,14 +23,10 @@ def main(stdscr): # stdscr == stand screen
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    # using those colors, positioniing them in the canvas
-    stdscr.addstr(1, 5, "Hello world", curses.color_pair(1))
-    stdscr.addstr(1, 0, "Hello world", curses.color_pair(2))
-    stdscr.refresh()
-    # grabbing what the user pressed
-    key=stdscr.getkey()
-    print(key)
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
+
 
 
 wrapper(main) 
