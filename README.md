7ce0ebe7c54e97fbc42c329b86e7c55a014c8aaa
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-09-14 20:42:38 -0500

    Change color during mistype

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Typing Bad!
+# Typing Bad!  
+
+## Consideration and Features  
+Use of ```curses.wrapper()``` function to restore terminal to its previous state without messing up.
--- main.py
+++ main.py
@@ -9,18 +9,26 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey() # waiting to press a key by the user
 
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    
+    for i, char in enumerate(current):
+        correct_char=target[i]
+        color=curses.color_pair(1)
+        if char != correct_char:
+            color=curses.color_pair(2)
+        
+        stdscr.addstr(0, i, char, color)
+
+
 def wpm_test(stdscr):
     target_text="Hello world this is some test text for this app!"
     current_text=[]
 
-    
     while True:
         stdscr.clear()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-        
+        display_text(stdscr, target_text, current_text)
         stdscr.refresh()
         
         key=stdscr.getkey()
@@ -43,6 +51,8 @@ def main(stdscr): # stdscr == stand screen
 
     start_screen(stdscr)
     wpm_test(stdscr)
+    
+    
 
 
 
